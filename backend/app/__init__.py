f175caeedae3630e915828a71a3a870c7178afbb
vkumar825 <vkumar01825@gmail.com>
2022-11-12 18:37:08 -0500

    set up basic flask app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,4 +8,3 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import routes, models
---
+++ backend/app/routes.py
@@ -0,0 +1,6 @@
+from backend.app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
